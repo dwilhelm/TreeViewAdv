0ae340b0960f0cedc9749cec36d480d0b4063e51
Gal Cohen <galc@users.noreply.github.com>
2018-02-09 19:06:23 +0200

    Update AutoRowHeightLayout.cs
    Avoid wasted enumeration of all row heights if the selection is on the header column for which the function is expected to return -1 (at the end of the enumeration). When dealing with large sets (several thousands lines) - the response time for tree sorting through column click is unreasonable due to this enumeration.

--- Aga.Controls/Tree/AutoRowHeightLayout.cs
+++ Aga.Controls/Tree/AutoRowHeightLayout.cs
@@ -118,6 +118,9 @@ namespace Aga.Controls.Tree
 		public int GetRowAt(Point point)
 		{
 			int py = point.Y - _treeView.ActualColumnHeaderHeight;
+			if (py < 0)
+				return -1; 
+				
 			int y = 0;
 			for (int i = _treeView.FirstVisibleRow; i < _treeView.RowCount; i++)
 			{
